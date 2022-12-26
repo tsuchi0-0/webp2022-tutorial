12c7ee2f156d1432402f8ea0f2e1254c35ebd9b0
tsuchi0-0 <94343134+tsuchi0-0@users.noreply.github.com>
2022-12-26 23:49:37 +0900

    Delete src directory

--- src/api.js
+++
@@ -1,7 +0,0 @@
-export async function fetchImages(chara) {
-    const response = await fetch(
-        `https://www.amiiboapi.com/api/amiibo/?name=${chara}`
-    );
-    const data = await response.json();
-    return data.amiibo;
-}
